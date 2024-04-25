4638e83e6fc09c3deb3d067fc7fd3d08148464b6
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-04-25 23:52:30 +0300

    ProductImage services added.

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/ProductImageServices/IProductImageService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.Catalog.Dtos.ProductImageDtos;
+
+namespace MultiShop.Catalog.Services.ProductImageServices;
+
+public interface IProductImageService
+{
+    Task<List<ResultProductImageDto>> GetAllProductImagesAsync();
+    Task<GetByIdProductImageDto> GetByIdProductImageAsync(string id);
+    Task CreateProductImageAsync(CreateProductImageDto createProductImageDto);
+    Task UpdateProductImageAsync(UpdateProductImageDto updateProductImageDto);
+    Task DeleteProductImageAsync(string id);
+}
