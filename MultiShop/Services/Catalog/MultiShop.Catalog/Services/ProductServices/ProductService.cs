dd229af8f09f97840b0cc8b1ecce9088a2ab593d
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-04-25 22:34:20 +0300

    Added product services.

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/ProductServices/ProductService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.ProductServices;
+
+public class ProductService : IProductService
+{
+    private readonly IMongoCollection<Product> _productCollection;
+    private readonly IMapper _mapper;
+
+    public ProductService(IMapper mapper, IDatabaseSettings databaseSettings)
+    {
+        var client = new MongoClient(databaseSettings.ConnectionString);
+        var database = client.GetDatabase(databaseSettings.DatabaseName);
+        _productCollection = database.GetCollection<Product>(databaseSettings.ProductCollectionName);
+        _mapper = mapper;
+    }
+
+    public async Task<List<ResultProductDto>> GetAllProductsAsync()
+    {
+        var values = await _productCollection.Find(x => true).ToListAsync();
+        return _mapper.Map<List<ResultProductDto>>(values);
+    }
+
+    public async Task<GetByIdProductDto> GetByIdProductAsync(string id)
+    {
+        var values = await _productCollection.Find<Product>(x => x.ProductID == id).FirstOrDefaultAsync();
+        return _mapper.Map<GetByIdProductDto>(values);
+    }
+    public async Task CreateProductAsync(CreateProductDto createProductDto)
+    {
+        var values = _mapper.Map<Product>(createProductDto);
+        await _productCollection.InsertOneAsync(values);
+    }
+    public async Task UpdateProductAsync(UpdateProductDto updateProductDto)
+    {
+        var values = _mapper.Map<Product>(updateProductDto);
+        await _productCollection.FindOneAndReplaceAsync(x => x.ProductID == updateProductDto.ProductID, values);
+    }
+
+    public async Task DeleteProductAsync(string id)
+    {
+        await _productCollection.DeleteOneAsync(x => x.ProductID == id);
+    }
+
+}
