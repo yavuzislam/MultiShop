7dd302ca3568e294be1643b9c6342e931bd912d0
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-04-25 22:06:32 +0300

    Added category services.

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.Catalog.Dtos.CategoryDtos;
+
+namespace MultiShop.Catalog.Services.CategoryServices;
+
+public interface ICategoryService
+{
+    Task<List<ResultCategoryDto>> GetAllCategoriesAsync();
+    Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id);
+    Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+    Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+    Task DeleteCategoryAsync(string id);
+}
