387d8b519f0c93f10600cd278aeed6eb36b2aa3d
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-04-26 01:33:06 +0300

    ProductID property added.

--- MultiShop/Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/GetByIdProductDetailDto.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/GetByIdProductDetailDto.cs
@@ -5,4 +5,5 @@ public class GetByIdProductDetailDto
     public string ProductDetailID { get; set; }
     public string ProductDescription { get; set; }
     public string ProductInfo { get; set; }
+    public string ProductID { get; set; }
 }
--- MultiShop/Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/UpdateProductDetailDto.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/UpdateProductDetailDto.cs
@@ -5,4 +5,5 @@ public class UpdateProductDetailDto
     public string ProductDetailID { get; set; }
     public string ProductDescription { get; set; }
     public string ProductInfo { get; set; }
+    public string ProductID { get; set; }
 }
