4f99424ad3c4a5ef054bfa62a77e9776b08387ab
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-04-25 19:32:12 +0300

    Added category and product entities.

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiShop.Catalog.Entities;
+
+public class ProductDetail
+{
+    public string ProductDetailID { get; set; }
+    public string ProductDescription { get; set; }
+    public string ProductInfo { get; set; }
+}
