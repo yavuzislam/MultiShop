f760d1140c4e9c6431688eb2767abad1bcb7f79c
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-04-26 16:45:17 +0300

    Address entity created.

---
+++ MultiShop/Services/Order/Core/MultiShop.Order.Domain/Entities/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Order.Domain.Entities;
+
+public class Address
+{
+    public int AddressID { get; set; }
+    public string UserID { get; set; }
+    public string District { get; set; }
+    public string City { get; set; }
+    public string Detail { get; set; }
+}
