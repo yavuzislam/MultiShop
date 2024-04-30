6c430a16b3a37254f2811f303adf85b1372872e9
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-04-30 13:20:55 +0300

    Create IRepository.cs

---
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace MultiShop.Order.Application.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<List<T>> GetAllAsync();
+    Task<T> GetBtIdAsync(int id);
+    Task<T> CreateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+    Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
+}
