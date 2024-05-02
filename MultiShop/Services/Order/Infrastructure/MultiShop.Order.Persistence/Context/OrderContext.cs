a3f9e6ee1d3fd4687695f05fa3da461bebe3662d
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-05-03 00:46:03 +0300

    Added Identity Server packages.

--- MultiShop/Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
+++ MultiShop/Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -7,7 +7,7 @@ public class OrderContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=localhost,;initial Catalog=MultiShopOrderDb;User=sa;Password=");
+        optionsBuilder.UseSqlServer("Server=localhost,1440;initial Catalog=MultiShopOrderDb;User=sa;Password=123456789.s5");
     }
 
     public DbSet<Address> Addresses { get; set; }
