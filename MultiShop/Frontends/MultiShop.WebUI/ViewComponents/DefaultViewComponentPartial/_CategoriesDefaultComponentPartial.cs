1b95fef2bafbb2e17832c3c565a3b8ac20e357a5
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-05-09 23:04:36 +0300

    Created User Interface Layout.

---
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponentPartial/_CategoriesDefaultComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponentPartial;
+
+public class _CategoriesDefaultComponentPartial:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/UILayoutComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutComponents;
+
+public class _FooterUILayoutComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
