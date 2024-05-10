a37c9c70decbe65524012bbe95b40f5fcae56e82
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-05-10 15:03:38 +0300

    "Designed the shopping cart and contact page"

---
+++ MultiShop/Frontends/MultiShop.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers;
+
+public class ContactController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/ShoppingCartViewComponents/_ShoppingCartProductListComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCartViewComponents;
+
+public class _ShoppingCartProductListComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
