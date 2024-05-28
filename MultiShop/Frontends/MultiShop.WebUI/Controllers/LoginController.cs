ba7f83752a0e63499ab49b975626b0dfc749e787
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-05-28 22:16:05 +0300

    Discovery Methods

--- MultiShop/Frontends/MultiShop.WebUI/Controllers/LoginController.cs
+++ MultiShop/Frontends/MultiShop.WebUI/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
 using MultiShop.WebUI.Models;
 using MultiShop.WebUI.Services;
+using MultiShop.WebUI.Services.Interfaces;
 using Newtonsoft.Json;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -17,11 +18,13 @@ public class LoginController : Controller
 {
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILoginService _loginService;
+    private readonly IIdentityService _identityService;
 
-    public LoginController(IHttpClientFactory httpClientFactory, ILoginService loginService)
+    public LoginController(IHttpClientFactory httpClientFactory, ILoginService loginService, IIdentityService identityService)
     {
         _httpClientFactory = httpClientFactory;
         _loginService = loginService;
+        _identityService = identityService;
     }
 
     [HttpGet]
@@ -67,5 +70,20 @@ public class LoginController : Controller
             }
         }
         return View();
+    }
+
+    //[HttpGet]
+    //public IActionResult SignIn()
+    //{
+    //    return View();
+    //}
+
+    //[HttpPost]
+    public async Task<IActionResult> SignIn(SignInDto signInDto)
+    {
+        signInDto.Username = "ali01";
+        signInDto.Password = "1111aA*";
+        await _identityService.SignIn(signInDto);
+        return RedirectToAction("Index", "Test");
     }
 }
--- MultiShop/Frontends/MultiShop.WebUI/Controllers/TestController.cs
+++ MultiShop/Frontends/MultiShop.WebUI/Controllers/TestController.cs
@@ -2,6 +2,7 @@
 using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
 
 namespace MultiShop.WebUI.Controllers;
 
@@ -16,33 +17,35 @@ public class TestController : Controller
 
     public async Task<IActionResult> Index()
     {
-
-        string token;
-        using (var httpClient=new HttpClient())
+        string token = "";
+        using (var httpClient = new HttpClient())
         {
             var request = new HttpRequestMessage
             {
                 RequestUri = new Uri("http://localhost:5001/connect/token"),
                 Method = HttpMethod.Post,
-                Content=new FormUrlEncodedContent(new Dictionary<string, string>
+                Content = new FormUrlEncodedContent(new Dictionary<string, string>
                 {
                     {"client_id","MultiShopVisitorId"},
                     {"client_secret","multishopsecret"},
                     {"grant_type","client_credentials"},
                 })
             };
-            using (var response=await httpClient.SendAsync(request))
+            using (var response = await httpClient.SendAsync(request))
             {
                 if (response.IsSuccessStatusCode)
                 {
                     var content = await response.Content.ReadAsStringAsync();
-                    var tokenResponse=JObject.Parse(content);
+                    var tokenResponse = JObject.Parse(content);
                     token = tokenResponse["access_token"].ToString();
                 }
             }
         }
 
+
         var client = _httpClientFactory.CreateClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
         var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories");
         if (responseMessage.IsSuccessStatusCode)
         {
@@ -50,6 +53,11 @@ public class TestController : Controller
             var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
             return View(values);
         }
+        return View();
+    }
+
+    public IActionResult Deneme1()
+    {
         return View();
     }
 }
