06a23c39eb24bbe1aa6057797a4aec2e45bb741b
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-05-30 01:28:24 +0300

    Client Credential Token Handler

---
+++ MultiShop/Frontends/MultiShop.WebUI/Handlers/ClientCredentialTokenHandler.cs
@@ -0,0 +1,27 @@
+﻿using MultiShop.WebUI.Services.Interfaces;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace MultiShop.WebUI.Handlers;
+
+public class ClientCredentialTokenHandler : DelegatingHandler
+{
+    private readonly IClientCredentialTokenService _clientCredentialTokenService;
+
+    public ClientCredentialTokenHandler(IClientCredentialTokenService clientCredentialTokenService)
+    {
+        _clientCredentialTokenService = clientCredentialTokenService;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var accessToken = await _clientCredentialTokenService.GetToken();
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        var response = await base.SendAsync(request, cancellationToken);
+        if (response.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            //hata mejası
+        }
+        return response;
+    }
+}
--- MultiShop/Frontends/MultiShop.WebUI/Program.cs
+++ MultiShop/Frontends/MultiShop.WebUI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MultiShop.WebUI.Handlers;
+using MultiShop.WebUI.Services.CatalogServices.CategoryServices;
 using MultiShop.WebUI.Services.Concrete;
 using MultiShop.WebUI.Services.Interfaces;
 using MultiShop.WebUI.Settings;
@@ -26,6 +27,8 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     opt.SlidingExpiration = true;
 });
 
+builder.Services.AddAccessTokenManagement();
+
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddScoped<ILoginService, LoginService>();
@@ -38,12 +41,20 @@ builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection("Cli
 builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings"));
 
 builder.Services.AddScoped<ResourceOwnerPasswordTokenHandler>();
+builder.Services.AddScoped<ClientCredentialTokenHandler>();
 
-var values=builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
+builder.Services.AddHttpClient<IClientCredentialTokenService, ClientCredentialTokenService>();
+
+var values = builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
 builder.Services.AddHttpClient<IUserService, UserService>(opt =>
 {
     opt.BaseAddress = new Uri(values.IdentityServerUrl);
 }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+
+builder.Services.AddHttpClient<ICategoryService, CategoryService>(opt =>
+{
+    opt.BaseAddress = new Uri($"{values.OcelotUrl}/{values.Catalog.Path}");
+}).AddHttpMessageHandler<ClientCredentialTokenHandler>();
 
 var app = builder.Build();
 
